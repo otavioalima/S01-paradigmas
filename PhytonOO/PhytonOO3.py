51f73d0fe8843a559081e2f6e012d5333d475002
Otavio Augusto Lima <otavioaugustosilvalima@gmail.com>
2025-10-16 20:11:00 -0300

    Add files via upload

---
+++ PhytonOO/PhytonOO3.py
@@ -0,0 +1,44 @@
+# Classe de Composicao
+class ArmaCorpoACorpo:
+    def __init__(self, tipo):
+        self.tipo = tipo
+
+    def __str__(self):
+        return f"Arma: {self.tipo}"
+
+
+# Classe Agregada
+class PhantomThieves:
+    def __init__(self, nome, arma):
+        self.nome = nome
+        self.arma = arma
+
+    def __str__(self):
+        return f"Membro: {self.nome} | Arma: {self.arma}"
+
+
+# Classe Principal (Joker)
+class Joker:
+    def __init__(self, membros):
+        # Composicao -> Joker cria e gerencia sua propria arma
+        self.arma = ArmaCorpoACorpo("Faca")
+        # Agregacao -> Recebe lista de membros PhantomThieves
+        self.membros = membros
+
+    # Metodo que mostra a equipe agregada
+    def mostrar_equipe(self):
+        print("Equipe dos Phantom Thieves")
+        print(f"Joker (Lider) | {self.arma}")
+        for membro in self.membros:
+            print(membro)
+
+
+# Execucao direta (sem verificacao __name__)
+membro1 = PhantomThieves("Skull", "Cano de ferro")
+membro2 = PhantomThieves("Panther", "Chicote")
+membro3 = PhantomThieves("Fox", "Katana")
+
+equipe = [membro1, membro2, membro3]
+joker = Joker(equipe)
+
+joker.mostrar_equipe()
